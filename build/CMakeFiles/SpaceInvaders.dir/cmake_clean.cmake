28c2082461a36d855586c58798d244b0675b9820
TheAssembler1 <lewisnoah592@gmail.com>
2022-04-02 11:46:17 -0500

    switched build systems

---
+++ build/CMakeFiles/SpaceInvaders.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpaceInvaders.dir/src/core/cpu.c.o"
+  "CMakeFiles/SpaceInvaders.dir/src/core/cpu_opcodes.c.o"
+  "CMakeFiles/SpaceInvaders.dir/src/core/manager.c.o"
+  "CMakeFiles/SpaceInvaders.dir/src/core/mem.c.o"
+  "CMakeFiles/SpaceInvaders.dir/src/entry.c.o"
+  "CMakeFiles/SpaceInvaders.dir/src/log/log.c.o"
+  "CMakeFiles/SpaceInvaders.dir/src/tests/tests.c.o"
+  "SpaceInvaders"
+  "SpaceInvaders.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SpaceInvaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
